0b3d250f3994832ce515c1a6235e05bc17ec6b0c
thisistaehun <taehun@tesser.co.kr>
2023-05-30 18:58:23 +0900

    adapt architecures

--- test/app.controller.spec.ts
+++ test/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../src/applications/controllers/app.controller';
+import { AppService } from '../src/applications/services/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
